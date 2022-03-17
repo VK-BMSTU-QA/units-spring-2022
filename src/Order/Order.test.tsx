3e2cedd75c94b737260ff557552141b208321e88
Kamila <nkf99@mail.ru>
2022-03-17 21:19:40 +0300

    mocked getDate and written unit-test for Order component

--- src/Order/Order.test.tsx
+++ src/Order/Order.test.tsx
@@ -1,7 +1,51 @@
 import React from 'react';
+import {OrderComponent} from './Order';
+import {shallow, configure} from 'enzyme';
+import {fakeOrders} from '../data/fakeOrders';
+import Adapter from 'enzyme-adapter-react-16';
+
+jest.mock('../utils/getDate');
+import {getDate} from '../utils/getDate';
+
+configure({ adapter: new Adapter() });
 
 describe('Order.tsx', () => {
-	it('some test', () => {
-		// test something here
+	beforeEach(() => {
+		(getDate as jest.Mock).mockReturnValue('some string');
+	});
+
+	afterAll(() => {
+		jest.resetModules();
+	});
+
+	it('getDate called', () => {
+		shallow(<OrderComponent order={fakeOrders[0]} />);
+
+		expect(getDate).toHaveBeenCalledTimes(1);
+	});
+
+	it('no items', () => {
+		const wrapper = shallow(<OrderComponent order={fakeOrders[0]} />);
+
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it('there are items in order', () => {
+		const wrapper = shallow(<OrderComponent order={fakeOrders[1]} />);
+
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it.each([
+		{
+			date: 12,
+		},
+		{
+			shop: 'shop',
+		}
+	])('returns null', (data) => {
+		const wrapper = shallow(<OrderComponent order={data} />);
+
+		expect(wrapper.getElement()).toBeNull();
 	});
 });

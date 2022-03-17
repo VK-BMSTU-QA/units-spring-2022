5b8760ec9e60a780284bbee3fd98c024d3ac2e56
Peshkov Dmitry <dmytrypeshkov@gmail.com>
2022-03-17 21:38:56 +0300

    units-spring: Тесты на Order

--- src/Order/Order.test.tsx
+++ src/Order/Order.test.tsx
@@ -1,7 +1,85 @@
+import {ShallowWrapper, shallow, configure} from 'enzyme';
 import React from 'react';
+import type {Order} from '../data/fakeOrders';
+
+import {OrderComponent} from './Order';
+import Adapter from 'enzyme-adapter-react-16';
+import {getDate} from '../utils/getDate';
+
+configure({ adapter: new Adapter() });
+
+jest.mock('../utils/getDate');
 
 describe('Order.tsx', () => {
-	it('some test', () => {
-		// test something here
+	let wrapper: ShallowWrapper;
+
+	afterEach(() => {
+		jest.clearAllMocks();
+	});
+
+	it('OrderComponent does not have "id" field', () => {
+		const order = {
+			date: 0,
+			shop: 'Shop0',
+			items: ['0'],
+		};
+		wrapper = shallow(<OrderComponent order={order} />);
+		expect(wrapper.getElement()).toBeNull();
+	});
+
+	it('OrderComponent does not have "date" field', () => {
+		const order = {
+			id: 1,
+			shop: 'Shop1',
+			items: ['1'],
+		};
+		wrapper = shallow(<OrderComponent order={order} />);
+		expect(wrapper.getElement()).toBeNull();
+	});
+
+	it('OrderComponent does not have "shop" field', () => {
+		const order = {
+			id: 2,
+			date: 2,
+			items: ['2'],
+		};
+		wrapper = shallow(<OrderComponent order={order} />);
+		expect(wrapper.getElement()).toBeNull();
+	});
+
+	it('OrderComponent does not have "items" field', () => {
+		const order = {
+			id: 3,
+			date: 3,
+			shop: 'Shop3',
+		};
+		wrapper = shallow(<OrderComponent order={order} />);
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it('render with all fields', () => {
+		const order: Order = {
+			id: 123,
+			date: 1544356800000,
+			shop: 'Alihandro Express',
+			items: [
+				'Утиный пластмасса для показ новый год',
+				'Курица из нержавеющей стали, утка, гусь, голубь, питьевой фонтан',
+				'Новый стиль один розница яйцо для упаковки форма латекс'
+			],
+		};
+		wrapper = shallow(<OrderComponent order={order} />);
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it('render with mocked getDate', () => {
+		const order = {
+			id: 4,
+			date: 4,
+			shop: 'Shop4',
+			items: ['4'],
+		};
+		wrapper = shallow(<OrderComponent order={order} />);
+		expect(getDate).toHaveBeenCalledTimes(1);
 	});
 });

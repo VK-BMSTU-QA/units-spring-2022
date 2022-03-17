e6d420578aabc74b4404754ff3140f34fb241f4c
Artyom Shirshov <artyomsh01@yandex.ru>
2022-03-17 21:30:21 +0300

    Ширшов Артём Тесты

--- src/Order/Order.test.tsx
+++ src/Order/Order.test.tsx
@@ -1,7 +1,79 @@
 import React from 'react';
+import {OrderComponent} from './Order';
+import {shallow, configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+jest.mock('../utils/getDate');
+import {getDate} from '../utils/getDate';
+
+configure({ adapter: new Adapter() });
+
 
 describe('Order.tsx', () => {
 	it('some test', () => {
 		// test something here
 	});
+});
+
+describe('Order.tsx', () => {
+	const wrapper;
+	getDate.mockReturnValue('1 января, чт, 1970 год');
+	const order: Order = {
+		id: 1,
+		date: 17032022,
+		shop: 'MegaShop',
+		items: ['Бургер', 'Кола']
+	};
+
+	const key = 0;
+
+	wrapper = shallow(<OrderComponent key = {key} order = {order}/>);
+
+	it('render with default state', () => {
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	
+
+});
+
+describe('Order.tsx', () => {
+	const wrapper;
+	getDate.mockReturnValue('1 января, чт, 1970 год');
+	const order: Order = {
+		id: 1,
+		date: 17032022,
+		shop: 'MegaShop',
+		items: {}
+	};
+
+	const key = 0;
+
+	wrapper = shallow(<OrderComponent key = {key} order = {order}/>);
+
+	it('no items', () => {
+		expect(wrapper).toMatchSnapshot();
+	});
+
+
+});
+
+describe('Order.tsx', () => {
+	const wrapper;
+	getDate.mockReturnValue('1 января, чт, 1970 год');
+	const order: Order = {
+		id: 1,
+		shop: 'MegaShop',
+		items: ['Бургер', 'Кола']
+	};
+
+	const key = 0;
+
+	wrapper = shallow(<OrderComponent key = {key} order = {order}/>);
+
+	it('no date', () => {
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	
+
 });
--- src/utils/sortOrders.test.ts
+++ src/utils/sortOrders.test.ts
@@ -1,4 +1,4 @@
-import {sortByItemCount} from './sortOrders';
+import {getSortFunction, sortByDate, sortByItemCount} from './sortOrders';
 
 describe('sortByItemCount function', () => {
 	it('same items count', () => {
@@ -15,4 +15,81 @@ describe('sortByItemCount function', () => {
 		expect(result).toBe(0);
 	});
 });
+
+describe('sortByItemСount function', () => {
+	it('different items count', () => {
+		const order1 = {
+			items: ['item1', 'item2', 'item3']
+		};
+
+		const order2 = {
+			items: ['1', '2'],
+		};
+
+		const result = sortByItemCount(order1,order2);
+		expect(result).toBe(1);
+	});
+});
+
+describe('sortByItemCount function', () => {
+	it('no items', () => {
+		const order1 = {};
+
+		const order2 = {};
+
+		const result = sortByItemCount(order1,order2);
+		expect(result).toBe(0);
+	});
+});
+
+describe('getSortFunction', () => {
+	it('not valid sortType', () => {
+		const sortType = 'number';
+		const result = getSortFunction(sortType);
+		expect(result).toBe(null);
+	});
+});
+
+describe('sortByDate function', () => {
+	it('same date', () => {
+		const order1 = {
+			date: 17032022
+		};
+
+		const order2 = {
+			date: 17032022
+		};
+
+		const result = sortByDate(order1,order2);
+		expect(result).toBe(0);
+	});
+});
+
+describe('sortByDate function', () => {
+	it('no date', () => {
+		const order1 = {};
+
+		const order2 = {};
+
+		const result = sortByDate(order1,order2);
+		expect(result).toBe(0);
+	});
+});
+
+/*
+describe('sortOrders function', () => {
+	it('no orders', () => {
+
+		const orders = {
+			orders: Order[]
+		}
+
+		const sortFunction = sortByDate
+
+		const result = sortOrders(orders,sortFunction)
+		expect(result).toBe(0)
+	})
+})
+*/
+
 

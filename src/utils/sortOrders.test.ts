a39093fe612d91e84ccf4e9a684727ecd2c0b48d
Georgiy <dev.georgiy@gmail.com>
2022-03-17 20:32:48 +0300

    90.62% покрытие setOrders

--- src/utils/sortOrders.test.ts
+++ src/utils/sortOrders.test.ts
@@ -1,4 +1,5 @@
-import {sortByDate, sortByItemCount} from './sortOrders';
+import {getSortFunction, sortByDate, sortByItemCount, sortOrders, sortTypes} from './sortOrders';
+import {Order} from "../data/fakeOrders";
 
 // Эту штуку пишем сами
 // Возможно придется обновить снапшоты
@@ -62,7 +63,7 @@ describe('sortByItemCount function', () => {
 });
 
 describe('sortByDate function', () => {
-	it('null items items count', () => {
+	it('equal items count', () => {
 		const order1 = {
 			date: 1,
 		};
@@ -74,5 +75,24 @@ describe('sortByDate function', () => {
 		const result = sortByDate(order1, order2);
 
 		expect(result).toBe(0);
+	});
+});
+
+describe('getSortFunction', () => {
+	it('wong type', () => {
+
+		const result = getSortFunction('wrong type');
+
+		expect(result).toBe(null);
+	});
+});
+
+describe('sortOrders', () => {
+	it('null length', () => {
+		const orders: Order[] = [];
+		const ordersOriginal: Order[] = [...orders];
+		sortOrders(orders, getSortFunction(sortTypes.DATE)); // Ругается т.к. getSortFunction может вернуть null..
+
+		expect(orders).toStrictEqual(ordersOriginal);
 	});
 });

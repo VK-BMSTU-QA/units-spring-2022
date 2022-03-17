6965a3bcea5e0808c2770356860adfc86fb31118
Александр Клонов <aleksandrklonov@MacBook-Air-Aleksandr.local>
2022-03-17 20:22:38 +0300

    90% coverage done

--- src/Order/Order.test.tsx
+++ src/Order/Order.test.tsx
@@ -1,7 +1,37 @@
+jest.mock('../utils/getDate');
 import React from 'react';
+import {getDate} from '../utils/getDate';
+
+import {shallow, configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { fakeOrders } from '../data/fakeOrders';
+import { OrderComponent } from './Order';
+configure({ adapter: new Adapter() });
 
 describe('Order.tsx', () => {
-	it('some test', () => {
-		// test something here
+	beforeEach(() => {
+		(getDate as jest.Mock).mockReturnValue('17 марта, чт, 2022 год');
+	});
+
+	afterEach(() => {
+		jest.clearAllMocks();
+	});
+
+	it('render order without items', () => {
+		const order = fakeOrders[0];
+
+		const wrapper = shallow(<OrderComponent
+			order={order}/>);
+		expect(wrapper).toMatchSnapshot();
+		expect(getDate).toBeCalled();
+	});
+
+	it('render order with items', () => {
+		const order = fakeOrders[1];
+
+		const wrapper = shallow(<OrderComponent
+			order={order}/>);
+		expect(wrapper).toMatchSnapshot();
+		expect(getDate).toBeCalled();
 	});
 });
--- src/utils/sortOrders.test.ts
+++ src/utils/sortOrders.test.ts
@@ -1,18 +1,75 @@
-import {sortByItemCount} from './sortOrders';
+import {getSortFunction, sortByDate, sortByItemCount, sortOrders, sortTypes} from './sortOrders';
 
-describe('sortByItemCount function', () => {
-	it('same items count', () => {
-		const order1 = {
-			items: ['item1', 'item2'],
-		};
+describe('sortOrders', ()=>{
+	it('test sorting', () => {
+		const orders = [
+			{date: 20},
+			{date: 5},
+			{date: 1}
+		];
 
-		const order2 = {
-			items: ['1', '2'],
-		};
+		const sortFunc = jest.fn();
+		sortOrders(orders, sortFunc);
+		expect(sortFunc).toBeCalled();
+	});
 
-		const result = sortByItemCount(order1, order2);
+	it('test orders not array', () => {
+		const orders = undefined;
 
-		expect(result).toBe(0);
+		const sortFunc = jest.fn();
+		const res = sortOrders(orders, sortFunc);
+		expect(res).toBeUndefined();
 	});
 });
 
+describe('test sortOrders function', () => {
+	it('sortByItemsCount', () => {
+		const sort = getSortFunction(sortTypes.COUNT);
+		expect(sort).toBe(sortByItemCount);
+	});
+	it('sortByDate', () => {
+		const sort = getSortFunction(sortTypes.DATE);
+		expect(sort).toBe(sortByDate);
+	});
+});
+
+describe('sortByItemCount function', () => {
+	test.each([
+		[['item1', 'item2'], ['1', '2'], 0],  //same items count
+		[['1', '2', '3'], [], 1],  // second order is empty and shorter
+		[undefined, ['item1', 'item2'], 0],  // order has undefined items
+		[['item1'], ['item1', 'item2'], -1]  // second order has more items
+	])('.sortByItemCount(%s, %s)', (items1, items2, expectedRes) => {
+		const order1 = {
+			items: items1
+		};
+	
+		const order2 = {
+			items: items2
+		};
+
+		const result = sortByItemCount(order1, order2);
+		expect(result).toBe(expectedRes);
+	});
+});
+
+
+describe('sortByDateCount function', () => {
+	test.each([
+		[5, 5, 0],  // equal
+		[1, 5, 1], // first is smaller
+		[31, 30, -1],  // first is bigger
+		[undefined, 2, 0]  // has undefind
+	])('.sortByDate(%i, %i)', (firstDate, secondDate, expectedRes) => {
+		const order1 = {
+			date: firstDate
+		};
+	
+		const order2 = {
+			date: secondDate
+		};
+
+		const result = sortByDate(order1, order2);
+		expect(result).toBe(expectedRes);
+	});
+});

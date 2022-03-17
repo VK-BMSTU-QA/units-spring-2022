153829b77a299d7e013b3feb972a3c70f1c13083
Leonid Belyaev <l.belyaev@vk.team>
2022-03-17 21:48:50 +0300

    Initial tests

--- src/Order/Order.test.tsx
+++ src/Order/Order.test.tsx
@@ -1,7 +1,73 @@
 import React from 'react';
+import {OrderComponent} from './Order';
+import {configure, shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
 
 describe('Order.tsx', () => {
-	it('some test', () => {
-		// test something here
+	it('Handles null order', () => {
+		const order = null;
+		const wrapper = shallow(<OrderComponent order={order}/>);
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it('Renders order with items: null', () => {
+		const order = {
+			id: 100,
+			date: 1647541075867,
+			shop: 'МАГАЗИН НА ДИВАНЕ',
+			items: null,
+		};
+		const wrapper = shallow(<OrderComponent order={order}/>);
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it('order with dict items', () => {
+		const order = {
+			id: 100,
+			date: 1647541075867,
+			shop: 'VK )',
+			items: {
+				apple_pen: 'Pencil',
+				minecraft: 'Mine',
+			},
+		};
+		const wrapper = shallow(<OrderComponent order={order}/>);
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it('Handles incomplete order', () => {
+		const order = {
+			id: 100,
+			items: [],
+		};
+		const wrapper = shallow(<OrderComponent order={order}/>);
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it('Renders empty order', () => {
+		const order = {
+			id: 1,
+			date: 1647541075867,
+			shop: 'AZOT',
+			items: [],
+		};
+		const wrapper = shallow(<OrderComponent order={order}/>);
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it('Renders correct order', () => {
+		const order = {
+			id: 1,
+			date: 1647541075867,
+			shop: 'SHOP',
+			items: [
+				'айфончик',
+				'гэлакси телефон'
+			],
+		};
+		const wrapper = shallow(<OrderComponent order={order}/>);
+		expect(wrapper).toMatchSnapshot();
 	});
 });
--- src/utils/sortOrders.test.ts
+++ src/utils/sortOrders.test.ts
@@ -1,4 +1,4 @@
-import {sortByItemCount} from './sortOrders';
+import {getSortFunction, sortByDate, sortByItemCount, sortOrders, sortTypes} from './sortOrders';
 
 describe('sortByItemCount function', () => {
 	it('same items count', () => {
@@ -14,5 +14,127 @@ describe('sortByItemCount function', () => {
 
 		expect(result).toBe(0);
 	});
+
+	it('Sorts orders with different item count', () => {
+		const order1 = {
+			items: ['itm1', 'itm2', 'itm3'],
+		};
+		
+		const order2 = {
+			items: ['1', '2'],
+		}
+
+		const result = sortByItemCount(order1, order2);
+		expect(result).toBe(1);
+
+		const reverseResult = sortByItemCount(order2, order1);
+		expect(reverseResult).toBe(-1);
+	});
+
+	it('Handles empty arrays', () =>{
+		const order1 = {
+			items: ['1', '2'],
+		}
+
+		const order2 = {
+			items: [],
+		}
+
+		const result = sortByItemCount(order1, order2);
+		expect(result).toBe(1)
+	});
+
+	it('Handles empty orders', () => {
+		const order1 = {
+		}
+		const order2 = {
+		}
+		const result = sortByItemCount(order1, order2);
+		expect(result).toBe(undefined);
+	})
+	
 });
 
+
+describe('sortByDate function', () => {
+	it('Handles empty order', () => {
+		const order1 = null;
+		const order2 = null;
+
+		const result = sortByDate(order1, order2);
+		expect(result).toBe(0);
+	});
+
+	it('Sorts items by numeric date', () =>{
+		const now = Date.now();
+		const later = now + 3600 * 1000;
+		
+		const order1 = {date: now}
+		const order2 = {date: later}
+
+		const result = sortByDate(order1, order2);
+		expect(result).toBe(1);
+
+		const reverseResult = sortByDate(order2, order1);
+		expect(reverseResult).toBe(-1);
+	});
+
+	it('Sorts orders with the same time', () => {
+		const now = Date.now();
+		const order1 = {date: now};
+		const order2 = {date: now};
+
+		const result = sortByDate(order1, order2);
+		
+		expect(result).toBe(0);
+	})
+});
+
+describe('sortOrders function', () => {
+	it('Sorts orders using sortByItemCount function', () => {
+		const order1 = {
+			items: ['item1', 'item2', 'item3'],
+		};
+
+		const order2 = {
+			items: ['1', '2'],
+		};
+
+		const result = sortOrders([order1, order2], sortByItemCount);
+		expect(result).toBe(1);
+	});
+
+	it('Sorts orders using sortByDate function', () => {
+		const now = Date.now();
+		const later = now + 3600 * 1000;
+		
+		const order1 = {date: now}
+		const order2 = {date: later}
+
+		const result = sortOrders([order1, order2], sortByDate);
+		expect(result).toBe(1);
+		
+		const reverseResult = sortOrders([order2, order1], sortByDate);
+		console.log(reverseResult);
+		expect(reverseResult).toBe(0);
+	})
+
+	it('Handles empty orders', () => {
+		const order1 = {};
+		const order2 = {};
+
+		const result = sortOrders([order1, order2], sortByDate);
+		expect(result).toBe(undefined);
+	})
+});
+
+describe('getSortFunction function', () => {
+	test.each([
+		{order: 'unknown', expected: null},
+		{order: sortTypes.DATE, expected: sortByDate},
+		{order: sortTypes.COUNT, expected: sortByItemCount}
+	])('Returns correct sort type', ({order, expected}) =>  {
+		const result = getSortFunction(order);
+		expect(result).toBe(expected);
+	})
+})

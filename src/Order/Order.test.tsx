93b6fbb49b300f57c820cc9c379eeaa56be3e840
TonyBlock <ant-chum@mail.ru>
2022-03-17 21:01:12 +0300

    покрыть модули тестами

--- src/Order/Order.test.tsx
+++ src/Order/Order.test.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import {OrderComponent} from './Order';
+import {shallow, configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
 
 describe('Order.tsx', () => {
-	it('some test', () => {
-		// test something here
+
+	it('all info', () => {
+		const someOrder = {
+			id: 123,
+			date: 1544356800000,
+			shop: 'Alihandro Express',
+			items: [
+				'Утиный пластмасса для показ новый год',
+				'Курица из нержавеющей стали, утка, гусь, голубь, питьевой фонтан',
+				'Новый стиль один розница яйцо для упаковки форма латекс',
+			]
+		};
+		const wrapper = shallow(<OrderComponent order={someOrder}/>);
+		expect(wrapper).toMatchSnapshot();
 	});
 });
--- src/utils/sortOrders.test.ts
+++ src/utils/sortOrders.test.ts
@@ -1,4 +1,43 @@
-import {sortByItemCount} from './sortOrders';
+import {getSortFunction, sortByDate, sortByItemCount, sortOrders, sortTypes} from './sortOrders';
+
+describe('sortOrders function', () => {
+	it('count sort type', () => {
+		const orders = [
+			{ items: ['item1', 'item2', 'item3'] },
+			{ items: ['1', '2'] },
+		];
+		const ordersCopy = [...orders];
+		sortOrders(orders, sortByItemCount);
+		expect(orders).not.toEqual(ordersCopy);
+	});
+
+	it('bad data', () => {
+		const orders = [
+			{ items: ['item1', 'item2', 'item3'] },
+			{ items: ['1', '2'] },
+		];
+		const ordersCopy = [...orders];
+		sortOrders(undefined, sortByItemCount);
+		expect(orders).toEqual(ordersCopy);
+	});
+});
+
+describe('getSortFunction function', () => {
+	it('count sort type', () => {
+		const result = getSortFunction(sortTypes.COUNT);
+		expect(result).toBe(sortByItemCount);
+	});
+
+	it('date sort type', () => {
+		const result = getSortFunction(sortTypes.DATE);
+		expect(result).toBe(sortByDate);
+	});
+
+	it('bad sort type', () => {
+		const result = getSortFunction('smth');
+		expect(result).toBeNull();
+	});
+});
 
 describe('sortByItemCount function', () => {
 	it('same items count', () => {
@@ -14,5 +53,120 @@ describe('sortByItemCount function', () => {
 
 		expect(result).toBe(0);
 	});
+
+	it('first order is bigger', () => {
+		const order1 = {
+			items: ['item1', 'item2', 'item3'],
+		};
+
+		const order2 = {
+			items: ['1', '2'],
+		};
+
+		const result = sortByItemCount(order1, order2);
+
+		expect(result).toBe(1);
+	});
+
+	it('second order is bigger', () => {
+		const order1 = {
+			items: ['item1', 'item2'],
+		};
+
+		const order2 = {
+			items: ['1', '2', '3'],
+		};
+
+		const result = sortByItemCount(order1, order2);
+
+		expect(result).toBe(-1);
+	});
+
+	it('null data', () => {
+		const order2 = {
+			items: ['1', '2', '3'],
+		};
+
+		const result = sortByItemCount(null, order2);
+
+		expect(result).toBe(0);
+	});
+
+	it('bad input data', () => {
+		const order1 = {};
+
+		const order2 = {
+			items: ['1', '2', '3'],
+		};
+
+		const result = sortByItemCount(order1, order2);
+
+		expect(result).toBe(0);
+	});
 });
 
+describe('sortByDate function', () => {
+	it('same date', () => {
+		const order1 = {
+			date: 123,
+		};
+
+		const order2 = {
+			date: 123,
+		};
+
+		const result = sortByDate(order1, order2);
+
+		expect(result).toBe(0);
+	});
+
+	it('first date is bigger', () => {
+		const order1 = {
+			date: 256,
+		};
+
+		const order2 = {
+			date: 123,
+		};
+
+		const result = sortByDate(order1, order2);
+
+		expect(result).toBe(-1);
+	});
+
+	it('second date is bigger', () => {
+		const order1 = {
+			date: 123,
+		};
+
+		const order2 = {
+			date: 256,
+		};
+
+		const result = sortByDate(order1, order2);
+
+		expect(result).toBe(1);
+	});
+
+	it('null data', () => {
+		const order2 = {
+			items: ['1', '2', '3'],
+		};
+
+		const result = sortByDate(null, order2);
+
+		expect(result).toBe(0);
+	});
+
+	it('bad input data', () => {
+		const order1 = {};
+
+		const order2 = {
+			date: 123,
+		};
+
+		const result = sortByDate(order1, order2);
+
+		expect(result).toBe(0);
+	});
+});

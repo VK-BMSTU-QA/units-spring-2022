cb1e372abfcf68eab2123b66f03152f36f708a61
Dmitriy Varin <dvvarin@avito.ru>
2022-03-17 21:17:30 +0300

    init tests for Order

--- src/Order/Order.test.tsx
+++ src/Order/Order.test.tsx
@@ -1,7 +1,33 @@
-import React from 'react';
+jest.mock('../utils/getDate');
+import { OrderComponent } from './Order';
+import {getDate} from "../utils/getDate"
 
 describe('Order.tsx', () => {
-	it('some test', () => {
-		// test something here
+	describe('Order.tsx', () => {
+		beforeEach(() => {
+			(getDate as jest.Mock).mockReturnValue('23 февраля, ср, 2022 год');
+		});
+	
+		afterEach(() => {
+			jest.clearAllMocks();
+		});
+	
+		test.each([
+			{
+				date: 5,
+				shop: undefined, 
+				items:[]
+			},
+			{
+				date: undefined, 
+				shop: 'amazon', 
+				items:[1, 2]
+			},
+		])('OrderComponent({order: %s})', (order) => {
+			const orderComponent = OrderComponent({order: order}); 
+			expect(orderComponent).toBeNull();
+		});
+	
+		
 	});
 });

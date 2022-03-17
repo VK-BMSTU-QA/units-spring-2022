2db738a4975c6399d283366dc8f998d9fd1f865d
Mikhail Popov <mikh.popov@vk.team>
2022-03-17 21:25:44 +0300

    done tests

--- src/Order/Order.test.tsx
+++ src/Order/Order.test.tsx
@@ -1,7 +1,70 @@
 import React from 'react';
+import { fakeOrders } from '../data/fakeOrders';
+import {shallow, configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import {OrderComponent, OrderComponentProps} from './Order';
+jest.mock('../utils/getDate');
+import {getDate} from '../utils/getDate';
+
+configure({ adapter: new Adapter() });
 
 describe('Order.tsx', () => {
-	it('some test', () => {
-		// test something here
+	let wrapper;
+
+	beforeEach(() => {
+		(getDate as jest.Mock).mockReturnValue('2022-03-17');
+	});
+
+	afterEach(() => {
+		jest.clearAllMocks();
+	});
+
+	it('render undefined order', () => {
+		wrapper = shallow(<OrderComponent key={0} order={undefined}/>);
+
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it('render order with undefined shop', () => {
+		const order = {
+			shop: undefined,
+			date: 1234,
+		};
+		wrapper = shallow(<OrderComponent key={0} order={order}/>);
+
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it('render order with undefined date', () => {
+		const order = {
+			shop: 'test',
+			date: undefined,
+		};
+		wrapper = shallow(<OrderComponent key={0} order={order}/>);
+
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it('render order with undefined items', () => {
+		const order = {
+			shop: 'test',
+			date: 1234,
+			items: undefined,
+		};
+		wrapper = shallow(<OrderComponent key={0} order={order}/>);
+
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it('render order with empty items', () => {
+		wrapper = shallow(<OrderComponent key={0} order={fakeOrders[0]}/>);
+
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it('render order with items', () => {
+		wrapper = shallow(<OrderComponent key={0} order={fakeOrders[1]}/>);
+
+		expect(wrapper).toMatchSnapshot();
 	});
 });
--- src/utils/sortOrders.test.ts
+++ src/utils/sortOrders.test.ts
@@ -1,4 +1,4 @@
-import {sortByItemCount} from './sortOrders';
+import {sortByItemCount, sortByDate, getSortFunction, sortTypes, sortOrders} from './sortOrders';
 
 describe('sortByItemCount function', () => {
 	it('same items count', () => {
@@ -13,6 +13,220 @@ describe('sortByItemCount function', () => {
 		const result = sortByItemCount(order1, order2);
 
 		expect(result).toBe(0);
+	});
+
+	it('first items count less', () => {
+		const order1 = {
+			items: ['item2'],
+		};
+
+		const order2 = {
+			items: ['1', '2'],
+		};
+
+		const result = sortByItemCount(order1, order2);
+
+		expect(result).toBe(-1);
+	});
+	
+	it('second items count less', () => {
+		const order1 = {
+			items: ['item1', 'item2'],
+		};
+
+		const order2 = {
+			items: ['2'],
+		};
+
+		const result = sortByItemCount(order1, order2);
+
+		expect(result).toBe(1);
+	});
+	
+	it('first struct is empty', () => {
+		const order1 = {};
+
+		const order2 = {
+			items: ['2'],
+		};
+
+		const result = sortByItemCount(order1, order2);
+
+		expect(result).toBe(0);
+	});
+	
+	it('second struct is empty', () => {
+		const order1 = {
+			items: ['item1', 'item2'],
+		};
+
+		const order2 = {};
+
+		const result = sortByItemCount(order1, order2);
+
+		expect(result).toBe(0);
+	});
+	
+	it('first struct with empty field', () => {
+		const order1 = undefined;
+
+		const order2 = {
+			items: ['1', '2'],
+		};
+
+		const result = sortByItemCount(order1, order2);
+
+		expect(result).toBe(0);
+	});
+	
+	it('first struct with empty field', () => {
+		const order1 = {
+			items: ['item1', 'item2'],
+		};
+
+		const order2 = undefined;
+
+		const result = sortByItemCount(order1, order2);
+
+		expect(result).toBe(0);
+	});
+});
+
+
+describe('sortByDate function', () => {
+	it('same date', () => {
+		const order1 = {
+			date: 1234,
+		};
+
+		const order2 = {
+			date: 1234,
+		};
+
+		const result = sortByDate(order1, order2);
+
+		expect(result).toBe(0);
+	});
+
+	it('first date less', () => {
+		const order1 = {
+			date: 1234,
+		};
+
+		const order2 = {
+			date: 1243,
+		};
+
+		const result = sortByDate(order1, order2);
+
+		expect(result).toBe(1);
+	});
+	
+	it('second date less', () => {
+		const order1 = {
+			date: 1243,
+		};
+
+		const order2 = {
+			date: 1234,
+		};
+
+		const result = sortByDate(order1, order2);
+
+		expect(result).toBe(-1);
+	});
+	
+	it('first struct is empty', () => {
+		const order1 = {};
+
+		const order2 = {
+			date: 1234,
+		};
+
+		const result = sortByDate(order1, order2);
+
+		expect(result).toBe(0);
+	});
+	
+	it('second struct is empty', () => {
+		const order1 = {
+			date: 1234,
+		};
+
+		const order2 = {};
+
+		const result = sortByDate(order1, order2);
+
+		expect(result).toBe(0);
+	});
+	
+	it('first struct undefined', () => {
+		const order1 = undefined;
+
+		const order2 = {
+			date: 1234,
+		};
+
+		const result = sortByDate(order1, order2);
+
+		expect(result).toBe(0);
+	});
+	
+	it('first struct undefined', () => {
+		const order1 = {
+			date: 1234,
+		};
+
+		const order2 = undefined;
+
+		const result = sortByDate(order1, order2);
+
+		expect(result).toBe(0);
+	});
+});
+
+describe('getSortFunction function', () => {
+	it('sort type items count', () => {
+		const result = getSortFunction(sortTypes.COUNT);
+
+		expect(result).toBe(sortByItemCount);
+	});
+
+	it('sort type date', () => {
+		const result = getSortFunction(sortTypes.DATE);
+
+		expect(result).toBe(sortByDate);
+	});
+
+	it('sort type undefined', () => {
+		const result = getSortFunction(undefined);
+
+		expect(result).toBeNull();
+	});
+});
+
+describe('sortOrders function', () => {
+	it('order is not array', () => {
+		const orders = [
+			{date: 1234},
+			{date: 4321},
+		];
+		const sortFunc = jest.fn();
+		sortOrders(orders, sortFunc);
+
+		expect(sortFunc).toBeCalled();
+	});
+
+	it('order is undefined', () => {
+		const result = sortOrders(undefined, jest.fn());
+
+		expect(result).toBeUndefined();
+	});
+
+	it('order is not array', () => {
+		const result = sortOrders(2, jest.fn());
+
+		expect(result).toBeUndefined();
 	});
 });
 

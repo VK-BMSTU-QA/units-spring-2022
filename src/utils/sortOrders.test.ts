c8ef281b9e7b9000538688faa53af450f0c215ef
Georgiy <dev.georgiy@gmail.com>
2022-03-17 20:45:48 +0300

    переписал тесты сортировок на each

--- src/utils/sortOrders.test.ts
+++ src/utils/sortOrders.test.ts
@@ -1,88 +1,33 @@
 import {getSortFunction, sortByDate, sortByItemCount, sortOrders, sortTypes} from './sortOrders';
 import {Order} from "../data/fakeOrders";
 
-// Эту штуку пишем сами
-// Возможно придется обновить снапшоты
-
 describe('sortByItemCount function', () => {
-	it('same items count', () => {
-		const order1 = {
-			items: ['item1', 'item2'],
-		};
-
-		const order2 = {
-			items: ['1', '2'],
-		};
-
+	it.each([
+		[{items: ['item1', 'item2']}, {items: ['item3', 'item4']}, 0],
+		[{items: ['item1', 'item2']}, {items: ['item3']}, 1],
+		[{items: ['item2']}, {items: ['item3', 'item4']}, -1],
+	])('test', (order1, order2, expected) => {
 		const result = sortByItemCount(order1, order2);
-
-		expect(result).toBe(0);
-	});
-
-	it('gt items count', () => {
-		const order1 = {
-			items: ['item1', 'item2'],
-		};
-
-		const order2 = {
-			items: ['1'],
-		};
-
-		const result = sortByItemCount(order1, order2);
-
-		expect(result).toBe(1);
-	});
-
-	it('lt items count', () => {
-		const order1 = {
-			items: ['item1'],
-		};
-
-		const order2 = {
-			items: ['1', '2'],
-		};
-
-		const result = sortByItemCount(order1, order2);
-
-		expect(result).toBe(-1);
-	});
-
-	it('null items items count', () => {
-		const order1 = {
-			items: null,
-		};
-
-		const order2 = {
-			items: ['1', '2'],
-		};
-
-		const result = sortByItemCount(order1, order2);
-
-		expect(result).toBe(0);
+		expect(result).toBe(expected);
 	});
 });
 
 describe('sortByDate function', () => {
-	it('equal items count', () => {
-		const order1 = {
-			date: 1,
-		};
-
-		const order2 = {
-			date: 1,
-		};
-
+	it.each([
+		[{date: 1}, {date: 1}, 0],
+		[{date: 1}, {date: 10}, 1],
+		[{date: 10}, {date: 1}, -1],
+		[{items: ['item1', 'item2']}, {items: ['item3']}, 1],
+		[{items: ['item2']}, {items: ['item3', 'item4']}, -1],
+	])('test', (order1, order2, expected) => {
 		const result = sortByDate(order1, order2);
-
-		expect(result).toBe(0);
+		expect(result).toBe(expected);
 	});
 });
 
 describe('getSortFunction', () => {
 	it('wong type', () => {
-
 		const result = getSortFunction('wrong type');
-
 		expect(result).toBe(null);
 	});
 });
@@ -92,7 +37,6 @@ describe('sortOrders', () => {
 		const orders: Order[] = [];
 		const ordersOriginal: Order[] = [...orders];
 		sortOrders(orders, getSortFunction(sortTypes.DATE)); // Ругается т.к. getSortFunction может вернуть null..
-
 		expect(orders).toStrictEqual(ordersOriginal);
 	});
 });

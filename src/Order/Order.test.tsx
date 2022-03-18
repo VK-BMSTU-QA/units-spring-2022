0241210abe4e161b3321eab09eb2d175390a03c1
Leonid Belyaev <l.belyaev@vk.team>
2022-03-18 11:43:49 +0300

    Mock date

--- src/Order/Order.test.tsx
+++ src/Order/Order.test.tsx
@@ -1,11 +1,22 @@
+jest.mock('../utils/getDate');
 import React from 'react';
 import {OrderComponent} from './Order';
 import {configure, shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import {getDate} from '../utils/getDate';
+import { fakeOrders } from '../data/fakeOrders';
 
 configure({ adapter: new Adapter() });
 
 describe('Order.tsx', () => {
+	console.log(getDate);
+	beforeEach(() => {
+		(getDate as jest.Mock).mockReturnValue('18 марта, чт, 2022 год');
+	});
+
+	afterEach(() => {
+		jest.clearAllMocks();
+	});
 	it('Handles null order', () => {
 		const order = null;
 		const wrapper = shallow(<OrderComponent order={order}/>);
@@ -33,26 +44,13 @@ describe('Order.tsx', () => {
 	});
 
 	it('Renders empty order', () => {
-		const order = {
-			id: 1,
-			date: 1647541075867,
-			shop: 'AZOT',
-			items: [],
-		};
+		const order = fakeOrders[0];
 		const wrapper = shallow(<OrderComponent order={order}/>);
 		expect(wrapper).toMatchSnapshot();
 	});
 
 	it('Renders correct order', () => {
-		const order = {
-			id: 1,
-			date: 1647541075867,
-			shop: 'SHOP',
-			items: [
-				'айфончик',
-				'гэлакси телефон'
-			],
-		};
+		const order = fakeOrders[1];
 		const wrapper = shallow(<OrderComponent order={order}/>);
 		expect(wrapper).toMatchSnapshot();
 	});
